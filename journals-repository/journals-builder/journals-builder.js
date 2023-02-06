fed9a813630b4c64746c0c7a427d29ba61814636
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-06 17:25:23 +0200

    journals-builder: implement read from disk

--- journals-repository/journals-builder/journals-builder.js
+++ journals-repository/journals-builder/journals-builder.js
@@ -1,5 +1,20 @@
+import fs from 'fs'
+
+function readJournalsFromDisk() {
+    let journals = [];
+    const journalsPath = "/home/dunnt/Documents/cleo-data/journals";
+    fs.readdir(journalsPath, (err, files) => {
+        console.log(files)
+        files.forEach(file => {
+            // Create a Journal using the file, and add it to the journals array
+        });
+    });
+    return journals;
+}
+
 export class JournalsBuilder {
     buildJournals() {
-        return ['silver']
+        let journals = [];
+        return journals.concat(readJournalsFromDisk());
     };
 }; 
