c4b947280451a630cee53751c8aac4d66ad04290
dunnt <tyronebdunn@gmail.com>
2023-02-06 11:55:52 +0200

    cleo: syntax fix

--- cleo.js
+++ cleo.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5011
+const express = require('express');
+const app = express();
+const port = 5011;
 
 app.get('/', (req, res) => {
-    res.send('Good day.')
-})
+    res.send('Good day.');
+});
 
 app.listen(port, () => {
-    console.log("Cleo here. I'm listening on port " + port)
-})
+    console.log("Cleo here. I'm listening on port " + port);
+});
