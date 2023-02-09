384491bdcb5f3665344e5ecb1ff042687d97dce4
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-09 14:47:39 +0200

    create-journal-file: refactor

--- src/create-journal-file/create-journal-file.ts
+++ src/create-journal-file/create-journal-file.ts
@@ -1,26 +1,21 @@
 const fs = require('fs');
-import { v4 as uuid } from "uuid";
-import { Journal } from "../journal/journal";
+import {v4 as uuid} from "uuid";
+import {Journal} from "../journal/journal";
 
 function getUniqueJournalID() {
     return uuid();
-};
+}
 
 function getUniqueJournal(name: string): Journal {
-    const journal = {"id": getUniqueJournalID(), "name": name};
-    return journal;
-};
+    return {"id": getUniqueJournalID(), "name": name};
+}
 
 export function createJournalFile(args: CreateJournalFileArgs) {
-    console.log("create journal stub.");
     const journal = getUniqueJournal(args.name);
-
-    console.log(journal);
-
     const ws = fs.createWriteStream(args.path + journal.id + '.cleo');
     ws.write(JSON.stringify(journal));
     ws.end();
-};
+}
 
 export type CreateJournalFileArgs = {
     name: string,
