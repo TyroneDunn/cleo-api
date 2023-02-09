28fa6cb3b44d474e7d9fb44618868dd69583c781
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-09 20:38:41 +0200

    journal-file-repository: inline method create journal file

--- src/journal-repository/journal-file-repository.ts
+++ src/journal-repository/journal-file-repository.ts
@@ -1,14 +1,11 @@
-import {createJournalFile} from "../create-journal-file/create-journal-file";
 import {Journal} from "../journal/journal";
 import {JournalRepository} from "./journal-repository";
 import {JournalsBuilder} from "./journals-builder/journals-builder";
+import {v4 as uuid} from "uuid";
+import * as fs from "fs";
 
 export class JournalsFileRepository implements JournalRepository {
-    deleteJournal(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
     journalPath = '/home/dunnt/Documents/cleo-data/journals/';
-
     async getJournals(): Promise<Journal[]> {
         return new JournalsBuilder().buildJournals(this.journalPath);
     };
@@ -20,7 +17,29 @@ export class JournalsFileRepository implements JournalRepository {
             });
     }
 
+    deleteJournal(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    //     get journal
+    //     if journal exists
+    //     delete journal file
+    }
+
     async createJournal(name: string): Promise<void> {
-        return Promise.resolve(createJournalFile({path: this.journalPath, name: name}));
+        const journal = this.getUniqueJournal(name);
+        const ws = fs.createWriteStream(this.journalPath + journal.id + '.cleo');
+        ws.write(JSON.stringify(journal));
+        ws.end();
+        return Promise.resolve();
     };
+
+    private getUniqueJournal(name: string): Journal {
+        return {
+            "id": this.getUniqueJournalID(),
+            "name": name
+        };
+    }
+
+    private getUniqueJournalID() {
+        return uuid();
+    }
 }
