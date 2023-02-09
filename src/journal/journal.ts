bcf3f26d4d7b9cdfc8be9bc0cff2a67fdfc895eb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-09 14:48:59 +0200

    journal: clean

--- src/journal/journal.ts
+++ src/journal/journal.ts
@@ -1,12 +1,3 @@
-// export class Journal {
-//     id;
-//     name;
-//     constructor(id, name) {
-//         this.id = id;
-//         this.name = name;
-//     }
-// };
-
 export type Journal = {
     id: string,
     name: string,
