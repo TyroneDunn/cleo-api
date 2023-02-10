09e3d1ffd6509501b6b26324c208820f205829f8
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-10 14:05:20 +0200

    journal-repository: fix get journal return value

--- src/journal-repository/journal-repository.ts
+++ src/journal-repository/journal-repository.ts
@@ -2,7 +2,7 @@ import { Journal } from "../journal/journal";
 
 export interface JournalRepository {
     getJournals(): Promise<Journal[]>;
-    getJournal(id: string): Promise<Journal>;
+    getJournal(id: string): Promise<Journal[]>;
     createJournal(name: string): Promise<void>;
     deleteJournal(id: string): Promise<void>;
 }
