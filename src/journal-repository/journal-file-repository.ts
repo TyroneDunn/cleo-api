a288ef816e56cac07ad136924b2391534afd9b4d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-10 14:03:03 +0200

    journal-file-repository: refactor and implement asnync read and write functions

--- src/journal-repository/journal-file-repository.ts
+++ src/journal-repository/journal-file-repository.ts
@@ -1,45 +1,97 @@
 import {Journal} from "../journal/journal";
 import {JournalRepository} from "./journal-repository";
-import {JournalsBuilder} from "./journals-builder/journals-builder";
-import {v4 as uuid} from "uuid";
 import * as fs from "fs";
+import {v4 as uuid} from "uuid";
 
 export class JournalsFileRepository implements JournalRepository {
     journalPath = '/home/dunnt/Documents/cleo-data/journals/';
     async getJournals(): Promise<Journal[]> {
-        return new JournalsBuilder().buildJournals(this.journalPath);
+        return this.readJournalsFromDisk();
     };
 
-    getJournal(id: string): Promise<Journal> {
-        return new JournalsBuilder().buildJournals(this.journalPath)
-            .filter((journal) => {
-                return journal.id === id;
-            });
+    private readJournalsFromDisk(): Promise<Journal[]> {
+        return new Promise<Journal[]>((resolve, reject) => {
+            fs.readdir(this.journalPath, (err, files) => {
+                if (err) {
+                    return reject();
+                } else {
+                    const fileData: Journal[] =
+                        files.filter((file) => {
+                            return (file !== '');
+                        }).map((filePath) => {
+                            fs.readFile(this.journalPath+filePath,(err, data) => {
+                                if (err) {
+                                    console.log('Error Reading Data from File Path: ', filePath, err);
+                                } else {
+                                    return data.toString();
+                                }
+                            });
+                            const jsonString = fs.readFileSync(this.journalPath + filePath).toString();
+                            return JSON.parse(jsonString);
+                        });
+                    return resolve(fileData);
+                }
+            })
+        });
     }
 
-    deleteJournal(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    //     get journal
-    //     if journal exists
-    //     delete journal file
+    async getJournal(id: string): Promise<Journal[]> {
+        return new Promise<Journal[]>((resolve, reject) => {
+            this.readJournalsFromDisk().then((journals) => {
+                const journal = journals.filter((journal) => {
+                    return journal.id === id;
+                });
+                if (!journal.length) {
+                    reject();
+                } else {
+                    resolve(journal);
+                }
+            })
+        });
     }
 
     async createJournal(name: string): Promise<void> {
-        const journal = this.getUniqueJournal(name);
-        const ws = fs.createWriteStream(this.journalPath + journal.id + '.cleo');
-        ws.write(JSON.stringify(journal));
-        ws.end();
-        return Promise.resolve();
-    };
+        const journal = this.createNewJournal(name);
+        return this.writeJournalToFile(journal);
+    }
 
-    private getUniqueJournal(name: string): Journal {
+    private writeJournalToFile(journal: Journal): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            try {
+                const ws = fs.createWriteStream(this.journalPath + journal.id + '.cleo');
+                ws.write(JSON.stringify(journal));
+                ws.end();
+                resolve();
+            } catch (err) {
+                reject();
+            }
+        });
+    }
+
+    private createNewJournal(name: string): Journal {
         return {
             "id": this.getUniqueJournalID(),
             "name": name
         };
     }
 
-    private getUniqueJournalID() {
+    private getUniqueJournalID(): string {
         return uuid();
     }
+
+    async deleteJournal(id: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            fs.unlink(this.journalPath+id+'.cleo', (err) => {
+                if (err) {
+                    reject();
+                } else {
+                    resolve();
+                }
+            })
+        });
+    }
 }
+
+
+
+
