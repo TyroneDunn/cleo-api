102b864ab600392e087457d8748c08e12a8feae2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-10 14:10:35 +0200

    request-handler: refactor, remove

--- src/request-handler/request-handler-factory.js
+++
@@ -1,3 +0,0 @@
-export function createHandler() {
-    return new RequestHandler();
-}
--- src/request-handler/request-handler.js
+++
@@ -1,6 +0,0 @@
-export class RequestHandler {
-    handleRequest(req) {
-        let res = 'mock response'
-        return res;
-    }
-};
