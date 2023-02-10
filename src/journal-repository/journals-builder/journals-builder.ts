e51ffd2ea82ee6db5218dd2717817b6488d57974
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-10 14:06:21 +0200

    journals-builder: refactor, removed redundancies

--- src/journal-repository/journals-builder/journals-builder.ts
+++
@@ -1,9 +0,0 @@
-const readJournalsFromDisk = require('../../read-journals-from-disk/read-journals-from-disk.ts');
-
-export class JournalsBuilder {
-    buildJournals(journalPath) {
-        const journalsWrapper = readJournalsFromDisk.readJournalsFromDisk(journalPath);
-        console.log('Wrapper Return Test: ' + journalsWrapper);
-        return journalsWrapper;
-    };
-}; 
