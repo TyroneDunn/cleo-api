f03e12715ef39beb43f2c88a1af38cbbffebc29c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-02-11 11:24:06 +0200

    journal-repository: added get entries

--- src/journal-repository/journal-repository.ts
+++ src/journal-repository/journal-repository.ts
@@ -6,6 +6,7 @@ export interface JournalRepository {
     getJournal(id: string): Promise<Journal[]>;
     createJournal(name: string): Promise<void>;
     deleteJournal(id: string): Promise<void>;
-    getEntry(id: string): Promise<JournalEntry[]>;
+    getEntry(journalid: string, id: string): Promise<JournalEntry[]>;
+    getEntries(journalid: string): Promise<JournalEntry[]>;
     createEntry(journalID: string, body: string): Promise<void>;
 }
