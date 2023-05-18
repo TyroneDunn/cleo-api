5959856a0cccf4b13f85c84b0d2560a0fdbdef2e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 10:56:05 +0200

    chore(journal-repository): reorder method list

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -2,8 +2,8 @@ import {Journal} from "./journal.type";
 import {Observable} from "rxjs";
 
 export interface JournalRepository {
-    journals$(userId: string): Observable<Journal[]>;
     journal$(id: string): Observable<Journal | undefined>;
+    journals$(userId: string): Observable<Journal[]>;
     createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
     updateJournal(id: string, name: string): Promise<Journal>;
