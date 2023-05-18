8334ab96e23d2bebc1cbfcea425df3595bb4f1da
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 12:23:34 +0200

    refactor(journal-repository): removed unused 'createJournal' method

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -5,7 +5,6 @@ export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
-    createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
     updateJournal(id: string, name: string): Promise<Journal>;
     journalExists(id: string): Promise<boolean>;
