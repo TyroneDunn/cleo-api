e281ef73b649c836b1587eec7c82115ee2b182c3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 11:56:28 +0200

    feat(journal-repository): define createJournal$ method template

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
+    createJournal$(userId: string, name: string): Observable<Journal>;
     createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
     updateJournal(id: string, name: string): Promise<Journal>;
