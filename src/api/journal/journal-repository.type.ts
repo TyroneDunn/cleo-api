ace24a1e23aa4096709f336b8a0a723832f20f23
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 11:05:19 +0200

    refactor(journal-repository): remove error return

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -2,7 +2,7 @@ import {Journal} from "./journal.type";
 import {Observable} from "rxjs";
 
 export interface JournalRepository {
-    journal$(id: string): Observable<Journal | Error>;
+    journal$(id: string): Observable<Journal>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
