6cf6f580a268910a02a570ad10db30015b4fdeeb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 11:02:49 +0200

    refactor(journal-repository): change a return type of journals$ from undefined type to error type

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -2,7 +2,7 @@ import {Journal} from "./journal.type";
 import {Observable} from "rxjs";
 
 export interface JournalRepository {
-    journal$(id: string): Observable<Journal | undefined>;
+    journal$(id: string): Observable<Journal | Error>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
