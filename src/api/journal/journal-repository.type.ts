8535aa207feb7733f891ba0d351d97910b093aa3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 12:58:37 +0200

    feat(journal-repository): add 'deleteJournal$' method template

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -5,6 +5,7 @@ export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
+    deleteJournal$(id: string): Observable<void>;
     deleteJournal(id: string): Promise<void>;
     updateJournal(id: string, name: string): Promise<Journal>;
     journalExists(id: string): Promise<boolean>;
