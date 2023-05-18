370792103344874b224922d2e3b103f2a9bf0711
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 13:00:54 +0200

    feat(journal-repository): add boolean return type to 'deleteJournal$' method template

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -5,7 +5,7 @@ export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
-    deleteJournal$(id: string): Observable<void>;
+    deleteJournal$(id: string): Observable<boolean>;
     updateJournal(id: string, name: string): Promise<Journal>;
     journalExists(id: string): Promise<boolean>;
 }
