018a8b26f09d42e6299429c6d683b27abbbcffc4
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-18 11:40:27 +0200

    feat(journal-repository): add undefined type as journal$ return option

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -2,7 +2,7 @@ import {Journal} from "./journal.type";
 import {Observable} from "rxjs";
 
 export interface JournalRepository {
-    journal$(id: string): Observable<Journal>;
+    journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
     createJournal(userId: string, name: string): Promise<Journal>;
     deleteJournal(id: string): Promise<void>;
