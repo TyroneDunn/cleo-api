1deb7d0afa04e2fee0253dd47eec267bcdd58c4f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-19 23:06:22 +0200

    feat(journal-repository): add 'updateJournal$' method definition

--- src/api/journal/journal-repository.type.ts
+++ src/api/journal/journal-repository.type.ts
@@ -6,6 +6,7 @@ export interface JournalRepository {
     journals$(userId: string): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
     deleteJournal$(id: string): Observable<Journal>;
+    updateJournal$(id: string, name: string): Observable<Journal>;
     updateJournal(id: string, name: string): Promise<Journal>;
     journalExists(id: string): Promise<boolean>;
 }
