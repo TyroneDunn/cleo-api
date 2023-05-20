099b3fab647d38d75b307b9d1ea0844e3a28dee9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-20 12:35:49 +0200

    feat(journal-entries-repository): add 'deleteEntry$' method definition

--- src/api/journal-entry/journal-entry-repository.type.ts
+++ src/api/journal-entry/journal-entry-repository.type.ts
@@ -5,6 +5,7 @@ export interface JournalEntryRepository {
     entry$(id: string): Observable<JournalEntry>;
     entries$(journalId: string): Observable<JournalEntry[]>;
     createEntry$(journalId: string, body: string): Observable<JournalEntry>;
+    deleteEntry$(journalId: string, entryId: string): Observable<void>;
     getEntry(id: string): Promise<JournalEntry>;
     getEntries(journalId: string): Promise<JournalEntry[]>;
     createEntry(journalId: string, body: string): Promise<JournalEntry>;
