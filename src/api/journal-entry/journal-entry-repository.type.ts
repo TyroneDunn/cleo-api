39310897ac1baa24d022d4904690f8e49b2c988f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-20 14:35:57 +0200

    refactor(journal-route): add undefined to 'entry$' return

--- src/api/journal-entry/journal-entry-repository.type.ts
+++ src/api/journal-entry/journal-entry-repository.type.ts
@@ -2,7 +2,7 @@ import {JournalEntry} from "./journal-entry.type";
 import {Observable} from "rxjs";
 
 export interface JournalEntryRepository {
-    entry$(id: string): Observable<JournalEntry>;
+    entry$(id: string): Observable<JournalEntry | undefined>;
     entries$(journalId: string): Observable<JournalEntry[]>;
     createEntry$(journalId: string, body: string): Observable<JournalEntry>;
     deleteEntry$(journalId: string, entryId: string): Observable<void>;
