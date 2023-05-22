01ac6cc9a412ded81fa4d2a3fbdc05c68783ce7c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-22 21:07:30 +0200

    feat(journal-repository): add 'sortUsersJournalsByLastUpdated$' method

--- src/journal/journal-repository.type.ts
+++ src/journal/journal-repository.type.ts
@@ -5,6 +5,7 @@ export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
     sortUsersJournalsByName$(id: string, order: 1 | -1): Observable<Journal[]>;
+    sortUsersJournalsByLastUpdated$(id: string, order: 1 | -1): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
     deleteJournal$(id: string): Observable<Journal>;
     updateJournal$(id: string, name: string): Observable<Journal>;
