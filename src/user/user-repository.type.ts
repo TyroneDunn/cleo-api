402653219af7ab8fa85736fbfb3294b303bb78c3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-22 09:26:30 +0200

    feat(user): add 'registerUser$' method definition

--- src/user/user-repository.type.ts
+++ src/user/user-repository.type.ts
@@ -1,4 +1,7 @@
+import {Observable} from "rxjs";
+
 export interface UserRepository {
+    registerUser$(username: string, password: string): Observable<void>;
     registerUser(username: string, password: string): Promise<void>;
     userExists(username: string): Promise<Boolean>;
 }
