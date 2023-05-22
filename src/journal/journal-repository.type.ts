e3aed4a6fd2787a745d4eac9fc58eb01ae140ba0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-22 20:42:27 +0200

    feat(journal-repository): add 'sortUsersJournalByName$' method definition

--- src/journal/journal-repository.type.ts
+++ src/journal/journal-repository.type.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 export interface JournalRepository {
     journal$(id: string): Observable<Journal | undefined>;
     journals$(userId: string): Observable<Journal[]>;
+    sortUsersJournalsByName$(id: string, order: 1 | -1): Observable<Journal[]>;
     createJournal$(userId: string, name: string): Observable<Journal>;
     deleteJournal$(id: string): Observable<Journal>;
     updateJournal$(id: string, name: string): Observable<Journal>;
