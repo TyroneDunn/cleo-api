cf2701645cf3571014b462200200d237ddf6d949
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-22 09:29:46 +0200

    refactor(user): add 'User' return type to 'registerUser$' method definition

--- src/user/user-repository.type.ts
+++ src/user/user-repository.type.ts
@@ -1,7 +1,8 @@
+import {User} from "./user.type";
 import {Observable} from "rxjs";
 
 export interface UserRepository {
-    registerUser$(username: string, password: string): Observable<void>;
+    registerUser$(username: string, password: string): Observable<User>;
     userExists$(username: string): Observable<Boolean>;
     registerUser(username: string, password: string): Promise<void>;
     userExists(username: string): Promise<Boolean>;
