9d30d651b2b9153c9d59245adb7ed2b488e44af1
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-22 09:26:48 +0200

    feat(user): add 'userExists$' method definition

--- src/user/user-repository.type.ts
+++ src/user/user-repository.type.ts
@@ -2,6 +2,7 @@ import {Observable} from "rxjs";
 
 export interface UserRepository {
     registerUser$(username: string, password: string): Observable<void>;
+    userExists$(username: string): Observable<Boolean>;
     registerUser(username: string, password: string): Promise<void>;
     userExists(username: string): Promise<Boolean>;
 }
