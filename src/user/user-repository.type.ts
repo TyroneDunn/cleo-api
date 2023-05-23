4b02b1803c615fa9b905644328722a6d90c937a8
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-23 17:59:01 +0200

    refactor(user-repository): remove 'registerUser' method definition

--- src/user/user-repository.type.ts
+++ src/user/user-repository.type.ts
@@ -4,5 +4,4 @@ import {Observable} from "rxjs";
 export interface UserRepository {
     registerUser$(username: string, password: string): Observable<User>;
     userExists$(username: string): Observable<boolean>;
-    registerUser(username: string, password: string): Promise<void>;
 }
