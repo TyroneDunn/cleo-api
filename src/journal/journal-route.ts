f41e790a04f12c61640c4880bedfec5e6cc5a5b8
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-23 12:39:38 +0200

    chore(journal-route): clean

--- src/journal/journal-route.ts
+++ src/journal/journal-route.ts
@@ -90,20 +90,6 @@ export class JournalRoute {
         }
 
         if (sort === 'name') {
-            if (parseInt(order) === 1) {
-                this.journalRepository.sortUsersJournalsByName$(
-                    ((req.user as User)._id as string),
-                    1
-                ).subscribe((journals) => {
-                    if (journals.length === 0) {
-                        res.status(NOT_FOUND)
-                            .json(`No journals found.`);
-                        return;
-                    }
-                    res.json(journals);
-                    return;
-                });
-        } else {
             this.journalRepository.sortUsersJournalsByName$(
                 ((req.user as User)._id as string),
                 order,
