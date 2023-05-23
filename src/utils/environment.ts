69f5792d174d5691cfd7917946ba7d934b1196eb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-23 22:19:16 +0200

    chore(mongoose-journal-repository): make types explicit

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,8 +1,8 @@
 require('dotenv').config();
-export const API_TITLE = process.env.TITLE;
-export const API_PORT = parseInt(process.env.CLEO_PORT);
-export const MONGO_DB_URL = process.env.MONGO_DB_URL;
-export const SESSION_SECRET = process.env.SESSION_SECRET;
-export const PASSWORD_SALT = process.env.PASSWORD_SALT;
-export const HASHING_ALGORITHM = process.env.HASING_ALGORITHM;
-export const HASHING_ITERATIONS = parseInt(process.env.HASHING_ITERATIONS);
+export const API_TITLE: string = process.env.TITLE;
+export const API_PORT: number = parseInt(process.env.CLEO_PORT);
+export const MONGO_DB_URL: string = process.env.MONGO_DB_URL;
+export const SESSION_SECRET: string = process.env.SESSION_SECRET;
+export const PASSWORD_SALT: string = process.env.PASSWORD_SALT;
+export const HASHING_ALGORITHM: string = process.env.HASING_ALGORITHM;
+export const HASHING_ITERATIONS: number = parseInt(process.env.HASHING_ITERATIONS);
