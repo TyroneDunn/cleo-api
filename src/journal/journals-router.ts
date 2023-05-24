3edf300e4e31e36fe86b311ea2ea16b86734cea2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-24 23:45:34 +0200

    feat(journal-router): implement 'searchUsersJournals$'

--- src/journal/journals-router.ts
+++ src/journal/journals-router.ts
@@ -4,6 +4,7 @@ import {userOwnsJournal$} from '../utils/userOwnsJournal$';
 import {
     journal$,
     journals$,
+    searchUsersJournals$,
     createJournal$,
     deleteJournal$,
     updateJournal$,
@@ -159,6 +160,67 @@ const getJournals: RequestHandler = async (req, res) => {
     }
 }
 
+const searchJournals: RequestHandler = (req, res) => {
+    const sort: string | undefined = req.query.sort as string;
+    const page: number = parseInt(req.query.page as string) || 1;
+    const limit: number = parseInt(req.query.limit as string) || 0;
+
+    if (!req.query.q) {
+        res.status(BAD_REQUEST)
+            .json('Query required.');
+        return;
+    }
+
+    if ((sort !== 'name') &&
+        (sort !== 'lastUpdated') &&
+        (sort !== 'dateCreated' &&
+            (sort !== undefined))) {
+        res.status(BAD_REQUEST)
+            .json('Invalid sort query.');
+        return;
+    }
+
+    if (((req.query.order as string) !== '1') &&
+        ((req.query.order as string) !== '-1') &&
+        ((req.query.order as string) !== undefined)) {
+        res.status(BAD_REQUEST)
+            .json('Invalid order query.');
+        return;
+    }
+
+    if (page < 0) {
+        res.status(BAD_REQUEST)
+            .json('Invalid page query.');
+        return;
+    }
+
+    if (limit < 0) {
+        res.status(BAD_REQUEST)
+            .json('Invalid limit query.');
+        return;
+    }
+
+    let order: 1 | -1;
+    if ((req.query.order as string) === '-1')
+        order = -1;
+    else
+        order = 1;
+
+    searchUsersJournals$(
+        ((req.user as User)._id as string),
+        req.query.q as string,
+        page,
+        limit
+    ).subscribe((journals) => {
+        if (journals.length === 0) {
+            res.status(NOT_FOUND)
+                .json('No journals found.');
+            return;
+        }
+        res.json(journals);
+    });
+};
+
 const createJournal: RequestHandler = async (req, res) => {
     if (!(req.body.name as string)){
         res.status(BAD_REQUEST)
@@ -238,6 +300,7 @@ const updateJournal: RequestHandler = async (req, res) => {
 const journalsRouter: Router = Router();
 journalsRouter.get('/:id', getJournal);
 journalsRouter.get('/', getJournals);
+journalsRouter.get('/search/', searchJournals);
 journalsRouter.post('/', createJournal);
 journalsRouter.delete('/:id', deleteJournal);
 journalsRouter.patch('/:id', updateJournal);
