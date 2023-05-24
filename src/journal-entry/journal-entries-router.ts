580439836f075d28f27ad9957c1812b1ed434b8e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-25 00:08:48 +0200

    feat(journal-entries-router): implement 'searchEntries$'

--- src/journal-entry/journal-entries-router.ts
+++ src/journal-entry/journal-entries-router.ts
@@ -3,6 +3,7 @@ import {User} from "../user/user.type";
 import {
     entry$,
     entries$,
+    searchEntries$,
     sortEntriesByLastUpdated$,
     sortEntriesByDateCreated$,
     createEntry$,
@@ -23,6 +24,68 @@ const getEntry: RequestHandler = async (req, res) => {
     if (!req.params.id) {
         res.status(BAD_REQUEST)
             .json('Entry id required.');
+        return;
+    }
+
+    if (req.params.id === 'search') {
+        const sort: string | undefined = req.query.sort as string;
+        const page: number = parseInt(req.query.page as string) || 1;
+        const limit: number = parseInt(req.query.limit as string) || 0;
+
+        if (!req.query.q) {
+            res.status(BAD_REQUEST)
+                .json('Query required.');
+            return;
+        }
+
+        if ((sort !== 'name') &&
+            (sort !== 'lastUpdated') &&
+            (sort !== 'dateCreated' &&
+                (sort !== undefined))) {
+            res.status(BAD_REQUEST)
+                .json('Invalid sort query.');
+            return;
+        }
+
+        if (((req.query.order as string) !== '1') &&
+            ((req.query.order as string) !== '-1') &&
+            ((req.query.order as string) !== undefined)) {
+            res.status(BAD_REQUEST)
+                .json('Invalid order query.');
+            return;
+        }
+
+        if (page < 0) {
+            res.status(BAD_REQUEST)
+                .json('Invalid page query.');
+            return;
+        }
+
+        if (limit < 0) {
+            res.status(BAD_REQUEST)
+                .json('Invalid limit query.');
+            return;
+        }
+
+        let order: 1 | -1;
+        if ((req.query.order as string) === '-1')
+            order = -1;
+        else
+            order = 1;
+
+        searchEntries$(
+            ((req.user as User)._id as string),
+            req.query.q as string,
+            page,
+            limit
+        ).subscribe((entries) => {
+            if (entries.length === 0) {
+                res.status(NOT_FOUND)
+                    .json('No entries found.');
+                return;
+            }
+            res.json(entries);
+        });
         return;
     }
 
