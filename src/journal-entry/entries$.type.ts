4cab97ab0f943442673598646003bc3e71bef3bb
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-24 16:26:34 +0200

    feat(entries$): add 'DeleteEntry$' type definition, and fix case errors

--- src/journal-entry/entries$.type.ts
+++ src/journal-entry/entries$.type.ts
@@ -1,27 +1,31 @@
 import {JournalEntry} from "./journal-entry.type";
 import {Observable} from "rxjs";
 
-export type entry$ = (id: string) => Observable<JournalEntry | undefined>;
+export type Entry$ = (id: string) => Observable<JournalEntry | undefined>;
 
-export type entries$ = (
+export type Entries$ = (
     id: string,
     page: number,
     limit: number,
 ) => Observable<JournalEntry[]>;
 
-export type createEntry$ = (
+export type CreateEntry$ = (
     id: string,
     body: string,
 ) => Observable<JournalEntry>;
 
-export type sortEntriesByDateCreated$ = (
+export type DeleteEntry$ = (
+    id: string,
+) => Observable<JournalEntry>;
+
+export type SortEntriesByDateCreated$ = (
     id: string,
     order: 1 | -1,
     page: number,
     limit: number,
 ) => Observable<JournalEntry[]>;
 
-export type sortEntriesByLastUpdated$ = (
+export type SortEntriesByLastUpdated$ = (
     id: string,
     order: 1 | -1,
     page: number,
