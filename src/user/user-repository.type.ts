50016d3f43c4d9aebd1c8e517adddfaa3f94e069
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-24 17:57:17 +0200

    refactor(user-repository): remove 'UserRepository' interface specification

--- src/user/user-repository.type.ts
+++
@@ -1,7 +0,0 @@
-import {User} from "./user.type";
-import {Observable} from "rxjs";
-
-export interface UserRepository {
-    registerUser$(username: string, password: string): Observable<User>;
-    userExists$(username: string): Observable<boolean>;
-}
