8d6eca79c22b9cc7298cbf33aa3c09fdfefee905
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-25 00:23:42 +0200

    chore(mongo-journals$): clean

--- src/journal/mongo-journals.ts
+++ src/journal/mongo-journals.ts
@@ -178,7 +178,7 @@ export const sortUsersJournalsByDateCreated$: SortUsersJournals$ = (
                 subscriber.complete();
             });
     });
-}
+};
 
 export const createJournal$: CreateJournal$ = (
     userId: string,
