ce9bd98cede5dadc3b2bffb6abc5ec87b62c3281
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 12:30:46 +0200

    chore(user-owns-journal): clean

--- src/utils/user-owns-journal.ts
+++ src/utils/user-owns-journal.ts
@@ -1,11 +1,8 @@
 import {Journal} from '../journal/journal.type';
 
-export const userOwnsJournal = (
-    userId: string,
-    journal: Journal | undefined,
-): boolean => {
+export const userOwnsJournal = (userId: string, journal: Journal | undefined): boolean => {
     if (!journal)
         return false;
 
     return(journal.author.toString() === userId);
-}
+};
