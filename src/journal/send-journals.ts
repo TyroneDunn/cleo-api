8e3aa2a7682cb132578f5679f38c260bb2de5432
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 11:48:06 +0200

    refactor(send-journals): export function

--- src/journal/send-journals.ts
+++ src/journal/send-journals.ts
@@ -2,7 +2,7 @@ import {Journal} from "./journal.type";
 import {Response} from "express";
 import {NOT_FOUND} from "../utils/http-status-constants";
 
-const sendJournals = (res: Response) => {
+export const sendJournals = (res: Response) => {
     return (journals: Journal[]): void => {
         if (journals.length === 0) {
             res.status(NOT_FOUND)
