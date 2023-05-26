050e2233398575ba736af1d647878f55c79bde5a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 15:45:06 +0200

    chore(mongo-users): clean
    Signed-off-by: Tyrone B. Dunn <tyronebdunn@gmail.com>

--- src/user/mongo-users.ts
+++ src/user/mongo-users.ts
@@ -4,40 +4,42 @@ import {RegisterUser$, UserExists$} from "./users.type";
 import {generateHash} from "../utils/password-utils";
 import {Observable} from "rxjs";
 
-export const registerUser$: RegisterUser$ = (username: string, password: string) => {
-    return new Observable((subscriber) => {
-        new UserModel({
-            username: username,
-            hash: generateHash(password),
-        }).save((error, user: User) => {
-            if (error) {
-                subscriber.error(error);
+export const registerUser$: RegisterUser$ =
+    (username: string, password: string): Observable<User> => {
+        return new Observable((subscriber) => {
+            new UserModel({
+                username: username,
+                hash: generateHash(password),
+            }).save((error, user: User) => {
+                if (error) {
+                    subscriber.error(error);
+                    subscriber.complete();
+                    return;
+                }
+                subscriber.next(user);
                 subscriber.complete();
-                return;
-            }
-            subscriber.next(user);
-            subscriber.complete();
+            });
         });
-    });
-};
+    };
 
-export const userExists$: UserExists$ = (username: string) => {
-    return new Observable((subscriber) => {
-        UserModel.findOne({username: username}, (error, user) => {
-            if (error) {
-                subscriber.error(error);
-                subscriber.complete();
-                return;
-            }
+export const userExists$: UserExists$ =
+    (username: string): Observable<boolean> => {
+        return new Observable((subscriber) => {
+            UserModel.findOne({username: username}, (error, user) => {
+                if (error) {
+                    subscriber.error(error);
+                    subscriber.complete();
+                    return;
+                }
 
-            if (!user) {
-                subscriber.next(false);
-                subscriber.complete();
-            }
-            else {
-                subscriber.next(true);
-                subscriber.complete();
-            }
+                if (!user) {
+                    subscriber.next(false);
+                    subscriber.complete();
+                }
+                else {
+                    subscriber.next(true);
+                    subscriber.complete();
+                }
+            });
         });
-    });
-};
+    };
