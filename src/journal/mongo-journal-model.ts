d5fd7f0edc51b3f4321570052b224052db21114a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 13:33:32 +0200

    refactor(mongo-journal-model): make journal document private
    Signed-off-by: Tyrone B. Dunn <tyronebdunn@gmail.com>

--- src/journal/mongo-journal-model.ts
+++ src/journal/mongo-journal-model.ts
@@ -2,7 +2,7 @@ import {Journal} from "./journal.type";
 import {Document, Schema} from 'mongoose';
 import database from "../utils/mongoose-database";
 
-export interface JournalDocument extends Document, Journal {
+interface JournalDocument extends Document, Journal {
   _id: string,
   name: string,
   author: Schema.Types.ObjectId;
