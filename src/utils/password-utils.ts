6157732946c7503acc079354e2dab6aa82710014
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 12:30:02 +0200

    chore(password-utils): clean

--- src/utils/password-utils.ts
+++ src/utils/password-utils.ts
@@ -1,9 +1,5 @@
-import {
-    PASSWORD_SALT,
-    HASHING_ITERATIONS,
-    PASSWORD_LENGTH,
-    HASHING_ALGORITHM
-} from "./environment";
+import {HASHING_ALGORITHM, HASHING_ITERATIONS, PASSWORD_LENGTH, PASSWORD_SALT} from "./environment";
+
 const crypto = require('crypto');
 
 const encrypt = (password: string): string =>
@@ -15,12 +11,11 @@ const encrypt = (password: string): string =>
         HASHING_ALGORITHM
     ).toString('hex');
 
-export const generateHash = (password: string): string => encrypt(password);
+export const generateHash = (password: string): string => 
+    encrypt(password);
 
-export const validatePassword = (password: string, hash: string): boolean => {
-    const comparator: string = encrypt(password);
-    return (hash === comparator);
-}
+export const validatePassword = (password: string, hash: string): boolean => 
+    (hash === encrypt(password));
 
 // Tool for the developer to generate new salt.
 // export const generateSalt = () => crypto.randomBytes(256).toString('hex');
