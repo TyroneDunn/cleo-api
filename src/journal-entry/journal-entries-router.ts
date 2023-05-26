98655c67f1e83bb95d9881fd506d19706e40770e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 15:41:03 +0200

    refactor(mongo-entries-request-handler): add 'getJournalEntries' request handler
    Signed-off-by: Tyrone B. Dunn <tyronebdunn@gmail.com>

--- src/journal-entry/journal-entries-router.ts
+++ src/journal-entry/journal-entries-router.ts
@@ -2,14 +2,16 @@ import {Router} from "express";
 import {
     searchEntries,
     getEntry,
+    getJournalEntries,
     createEntry,
     deleteEntry,
     updateEntry
-} from "./mongo-entries-request-handler";
+} from "./mongo-entries-request-handler"
 
 const journalEntriesRouter: Router = Router();
 journalEntriesRouter.get('/search/', searchEntries);
 journalEntriesRouter.get('/:id', getEntry);
+journalEntriesRouter.get('', getJournalEntries);
 journalEntriesRouter.post('/:id', createEntry);
 journalEntriesRouter.delete('/:id', deleteEntry);
 journalEntriesRouter.patch('/:id', updateEntry);
