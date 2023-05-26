474033ac8ba0ed409a85d04b618f895cce67528c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 15:46:28 +0200

    refactor(users): remove type definition
    Signed-off-by: Tyrone B. Dunn <tyronebdunn@gmail.com>

--- src/user/mongo-users.ts
+++ src/user/mongo-users.ts
@@ -1,45 +1,42 @@
 import {User} from "./user.type";
 import UserModel from "./mongo-user-model";
-import {RegisterUser$, UserExists$} from "./users.type";
 import {generateHash} from "../utils/password-utils";
 import {Observable} from "rxjs";
 
-export const registerUser$: RegisterUser$ =
-    (username: string, password: string): Observable<User> => {
-        return new Observable((subscriber) => {
-            new UserModel({
-                username: username,
-                hash: generateHash(password),
-            }).save((error, user: User) => {
-                if (error) {
-                    subscriber.error(error);
-                    subscriber.complete();
-                    return;
-                }
-                subscriber.next(user);
+export const registerUser$ = (username: string, password: string): Observable<User> => {
+    return new Observable((subscriber) => {
+        new UserModel({
+            username: username,
+            hash: generateHash(password),
+        }).save((error, user: User) => {
+            if (error) {
+                subscriber.error(error);
                 subscriber.complete();
-            });
+                return;
+            }
+            subscriber.next(user);
+            subscriber.complete();
         });
-    };
+    });
+};
 
-export const userExists$: UserExists$ =
-    (username: string): Observable<boolean> => {
-        return new Observable((subscriber) => {
-            UserModel.findOne({username: username}, (error, user) => {
-                if (error) {
-                    subscriber.error(error);
-                    subscriber.complete();
-                    return;
-                }
+export const userExists$ = (username: string): Observable<boolean> => {
+    return new Observable((subscriber) => {
+        UserModel.findOne({username: username}, (error, user) => {
+            if (error) {
+                subscriber.error(error);
+                subscriber.complete();
+                return;
+            }
 
-                if (!user) {
-                    subscriber.next(false);
-                    subscriber.complete();
-                }
-                else {
-                    subscriber.next(true);
-                    subscriber.complete();
-                }
-            });
+            if (!user) {
+                subscriber.next(false);
+                subscriber.complete();
+            }
+            else {
+                subscriber.next(true);
+                subscriber.complete();
+            }
         });
-    };
+    });
+};
