4f11dc463cc359e7341128b0005eed13b080020d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-26 09:29:31 +0200

    refactor(authGuard): make arrow function

--- src/user/auth-guard.ts
+++ src/user/auth-guard.ts
@@ -1,7 +1,7 @@
 import {RequestHandler} from "express";
 import {UNAUTHORIZED} from "../utils/http-status-constants";
 
-export function authGuard(req, res, next): RequestHandler {
+export const authGuard = (req, res, next): RequestHandler => {
     if (!req.isAuthenticated())
         return res.status(UNAUTHORIZED).json('Unauthorized.');
 
