e2818366f1f77cc3e393c31b095bcb749053cf42
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-05-31 12:34:00 +0200

    feat(journals-repository): define repository module and argument types

---
+++ src/journal/journals-repository.ts
@@ -0,0 +1,38 @@
+import {Journal} from "./journal.type";
+
+export type QueryArgs = {
+    id?: string,
+    idRegex?: string,
+    name?: string,
+    nameRegex?: string,
+    author?: string,
+    authorRegex?: string,
+};
+
+export type SortArgs = {
+    sort?: 'id' | 'name' | 'author' | 'dateCreated' | 'lastUpdated',
+    order?: 1 | -1,
+}
+
+export type FilterArgs = {
+    startDate?: Date,
+    endDate?: Date,
+};
+
+export type PaginationArgs = {
+    page: number,
+    limit: number,
+};
+
+export type JournalsRepository = {
+    getJournal: (args: QueryArgs) => Promise<Journal>,
+    getJournals: (
+        queryArgs: QueryArgs,
+        sortArgs: SortArgs,
+        filterArgs: FilterArgs,
+        paginationArgs: PaginationArgs,
+    ) => Promise<Journal[]>,
+    createJournal: (args: QueryArgs) => Promise<Journal>,
+    deleteJournal: (args: QueryArgs) => Promise<Journal>,
+    updateJournal: (args: QueryArgs) => Promise<Journal>,
+};
