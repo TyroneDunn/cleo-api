f27f489e9b30e3c1548579db37f707530aae933c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 17:03:13 +0200

    refactor(entries-controller): declare controller

---
+++ src/entry/entries-controller.ts
@@ -0,0 +1,7 @@
+import {EntriesRepository} from "./entries-repository";
+import {ENTRIES_REPOSITORY} from "../config";
+const repository: EntriesRepository = ENTRIES_REPOSITORY;
+
+export const EntriesController = {
+
+};
