23755f395d25c8f31ac89598fa623d376cea232c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 14:49:21 +0200

    feat(entries-dtos): define entries DTO types

---
+++ src/entry/entry-dtos.ts
@@ -0,0 +1,36 @@
+export type GetEntryDTO = {
+    userId: string,
+    id: string,
+};
+
+export type GetEntriesDTO = {
+    userId: string,
+    idRegex?: string,
+    body?: string,
+    bodyRegex?: string,
+    journal?: string,
+    journalRegex?: string,
+    sort?: 'id' | 'name' | 'author' | 'dateCreated' | 'lastUpdated',
+    order?: 1 | -1,
+    startDate?: Date,
+    endDate?: Date,
+    page?: number,
+    limit?: number,
+};
+
+export type CreateEntryDTO = {
+    userId: string,
+    journal: string,
+    name: string,
+};
+
+export type DeleteEntryDTO = {
+    userId: string,
+    id: string,
+};
+
+export type UpdateEntryDTO = {
+    userId: string,
+    id: string,
+    body: string,
+};
