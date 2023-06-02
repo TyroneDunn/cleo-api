08494e50999c344325956d8e8fb2210fd714e9d6
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 20:05:07 +0200

    feat(entries-dto-validator): implement validate functions

--- src/entry/entries-dto-validator.ts
+++ src/entry/entries-dto-validator.ts
@@ -7,7 +7,7 @@ import {
 } from "./entries-dtos";
 import {ValidationResult} from "../utils/validation-result";
 import {BadRequestError, NotFoundError, UnauthorizedError} from "../utils/errors";
-import {ENTRIES_REPOSITORY} from "../config";
+import {ENTRIES_REPOSITORY, JOURNALS_REPOSITORY} from "../config";
 
 export const validateGetEntryDTO = async (dto: GetEntryDTO): Promise<ValidationResult> => {
     if (!dto.userId)
@@ -22,17 +22,50 @@ export const validateGetEntryDTO = async (dto: GetEntryDTO): Promise<ValidationR
 };
 
 export const validateGetEntriesDTO = async (dto: GetEntriesDTO): Promise<ValidationResult> => {
-
+    if ((dto.body && dto.bodyRegex) ||
+        (dto.body && dto.bodyRegex) ||
+        (dto.journal && dto.journalRegex)) {
+        return {status: false, error: new BadRequestError('Invalid query.')};
+    }
+    return {status: true};
 };
 
 export const validateCreateEntryDTO = async (dto: CreateEntryDTO): Promise<ValidationResult> => {
-
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!dto.journal)
+        return {status: false, error: new BadRequestError('Journal required.')};
+    if (!dto.body)
+        return {status: false, error: new BadRequestError('Body required.')};
+    if (!(await JOURNALS_REPOSITORY.exists({id: dto.journal})))
+        return {status: false, error: new NotFoundError(`Journal ${dto.journal} not found.`)};
+    if (!(await JOURNALS_REPOSITORY.ownsJournal({author: dto.userId, id: dto.journal})))
+        return {status: false, error: new UnauthorizedError(`Unauthorized access to journal ${dto.journal}`)};
+    return {status: true};
 };
 
 export const validateDeleteEntryDTO = async (dto: DeleteEntryDTO): Promise<ValidationResult> => {
-
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!dto.id)
+        return {status: false, error: new BadRequestError('Entry ID required.')};
+    if (!(await ENTRIES_REPOSITORY.exists({id: dto.id})))
+        return {status: false, error: new NotFoundError(`Entry ${dto.id} not found.`)};
+    if (!(await ENTRIES_REPOSITORY.ownsEntry({userId: dto.userId, id: dto.id})))
+        return {status: false, error: new UnauthorizedError(`Unauthorized access to entry ${dto.id}`)};
+    return {status: true};
 };
 
 export const validateUpdateEntryDTO = async (dto: UpdateEntryDTO): Promise<ValidationResult> => {
-
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!dto.id)
+        return {status: false, error: new BadRequestError('Entry ID required.')};
+    if ((!dto.body) && (!dto.journal))
+        return {status: false, error: new BadRequestError('Update field required.')};
+    if (!(await ENTRIES_REPOSITORY.exists({id: dto.id})))
+        return {status: false, error: new NotFoundError(`Entry ${dto.id} not found.`)};
+    if (!(await ENTRIES_REPOSITORY.ownsEntry({userId: dto.userId, id: dto.id})))
+        return {status: false, error: new UnauthorizedError(`Unauthorized access to entry ${dto.id}`)};
+    return {status: true};
 };
