804f8527689fe33f0013b1f0e36403450e89cee2
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 13:23:02 +0200

    refactor(journals-controller): extract building repository arguments to functions

--- src/journal/journals-controller.ts
+++ src/journal/journals-controller.ts
@@ -22,6 +22,45 @@ import {
     UpdateJournalDTO
 } from "./journal-dtos";
 
+const buildGetJournalsQueryArgs = (dto: GetJournalsDTO) => {
+    let queryArgs: QueryArgs = {author: dto.userId}
+    if (dto.idRegex)
+        queryArgs.idRegex = dto.idRegex
+    if (dto.author)
+        queryArgs.author = dto.author
+    if (dto.authorRegex)
+        queryArgs.authorRegex = dto.authorRegex
+    if (dto.name)
+        queryArgs.name = dto.name
+    if (dto.nameRegex)
+        queryArgs.nameRegex = dto.nameRegex
+    return queryArgs;
+};
+
+function buildGetJournalsSortArgs(dto: GetJournalsDTO) {
+    const sortArgs: SortArgs = {};
+    if (dto.sort)
+        sortArgs.sort = dto.sort
+    dto.order ? sortArgs.order = dto.order : sortArgs.order = 1;
+    return sortArgs;
+}
+
+function buildGetJournalsFilterArgs(dto: GetJournalsDTO) {
+    const filterArgs: FilterArgs = {};
+    if (dto.startDate)
+        filterArgs.startDate = dto.startDate;
+    if (dto.endDate)
+        filterArgs.endDate = dto.endDate;
+    return filterArgs;
+}
+
+function buildPaginationArgs(dto: GetJournalsDTO) {
+    const paginationArgs: PaginationArgs = {};
+    dto.page ? paginationArgs.page = dto.page : paginationArgs.page = 1;
+    dto.limit ? paginationArgs.limit = dto.limit : paginationArgs.limit = 32;
+    return paginationArgs;
+}
+
 export const JournalsController = {
     getJournal: async (dto: GetJournalDTO): Promise<Journal> => {
         const validationResult: ValidationResult = await validateGetJournalDTO(dto);
@@ -36,18 +75,10 @@ export const JournalsController = {
         const validationResult: ValidationResult = await validateGetJournalsDTO(dto);
         if (!validationResult.status)
             throw validationResult.error;
-
-        const queryArgs: QueryArgs = {
-            id: dto.id,
-            name: dto.name,
-            author: dto.author,
-            idRegex: dto.idRegex,
-            nameRegex: dto.nameRegex,
-            authorRegex: dto.authorRegex,
-        }
-        const sortArgs: SortArgs = {sort: dto.sort, order: dto.order};
-        const filterArgs: FilterArgs = {startDate: dto.startDate, endDate: dto.endDate};
-        const paginationArgs: PaginationArgs = {page: dto.page, limit: dto.limit};
+        const queryArgs = buildGetJournalsQueryArgs(dto);
+        const sortArgs = buildGetJournalsSortArgs(dto);
+        const filterArgs = buildGetJournalsFilterArgs(dto);
+        const paginationArgs = buildPaginationArgs(dto);
         return repository.getJournals(queryArgs, sortArgs, filterArgs, paginationArgs);
     },
     
