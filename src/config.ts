6e4951571badc10202dfe5fa0d0e7206211896fd
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 23:36:54 +0200

    feat(config): add mongo users repository dependency

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,9 @@ import {MongoJournalsRepository} from "./journal/mongo-journals-repository";
 import {JournalsRepository} from "./journal/journals-repository";
 import {EntriesRepository} from "./entry/entries-repository";
 import {MongoEntriesRepository} from "./entry/mongo-entries-repository";
+import {UsersRepository} from "./user/users-repository";
+import {MongoUsersRepository} from "./user/mongo-users-repository";
 
+export const USERS_REPOSITORY: UsersRepository = MongoUsersRepository;
 export const JOURNALS_REPOSITORY: JournalsRepository = MongoJournalsRepository;
 export const ENTRIES_REPOSITORY: EntriesRepository = MongoEntriesRepository;
