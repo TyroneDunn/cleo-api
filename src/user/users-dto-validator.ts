2b660e853db8d6d48252ea3f445598ee5b5f72fe
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 23:31:31 +0200

    feat(users-dtos-validator): implement validator module

---
+++ src/user/users-dto-validator.ts
@@ -0,0 +1,17 @@
+import {RegisterUserDTO} from "./users-dtos";
+import {ValidationResult} from "../utils/validation-result";
+import {BadRequestError} from "../utils/errors";
+
+export const validateRegisterUserDTO = async (dto: RegisterUserDTO): Promise<ValidationResult> => {
+    if (!dto.username)
+        return {status: false, error: new BadRequestError('Username required.')};
+    if (!dto.password)
+        return {status: false, error: new BadRequestError('Password required.')};
+    return {status: true};
+};
+
+export const validateExistsDTO = async (dto: RegisterUserDTO): Promise<ValidationResult> => {
+    if (!dto.username)
+        return {status: false, error: new BadRequestError('Username required.')};
+    return {status: true};
+};
