4c7c288ef08031514a285be3b999227634f89d5f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-03 00:06:14 +0200

    feat(users-dtos): define user DTO types

--- src/user/users-dtos.ts
+++ src/user/users-dtos.ts
@@ -1,8 +1,34 @@
+export type GetUserDTO = {
+    userId: string,
+    id: string,
+};
+
+export type GetUsersDTO = {
+    userId: string,
+    id?: string,
+    idRegex?: string,
+    username?: string,
+    usernameRegex: string,
+    sort?: 'id' | 'username' | 'dateCreated' | 'lastUpdated',
+    order?: 1 | -1,
+    startDate?: Date,
+    endDate?: Date,
+    page?: number,
+    limit?: number,
+};
+
 export type RegisterUserDTO = {
     username: string,
     password: string,
 };
 
-export type ExistsDTO = {
-    username: string,
+export type DeleteUserDTO = {
+    userId: string,
+    id: string,
+};
+
+export type UpdateUserDTO = {
+    userId: string,
+    username?: string,
+    password?: string,
 };
