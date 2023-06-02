6fbdca263f1601d11d6cf6e2a74aa30513eedf5a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 23:47:38 +0200

    refactor(users-dtos-validator): add exists check to register user DTO validation

--- src/user/users-dto-validator.ts
+++ src/user/users-dto-validator.ts
@@ -1,12 +1,15 @@
 import {ExistsDTO, RegisterUserDTO} from "./users-dtos";
 import {ValidationResult} from "../utils/validation-result";
-import {BadRequestError} from "../utils/errors";
+import {BadRequestError, ConflictError} from "../utils/errors";
+import {USERS_REPOSITORY} from "../config";
 
 export const validateRegisterUserDTO = async (dto: RegisterUserDTO): Promise<ValidationResult> => {
     if (!dto.username)
         return {status: false, error: new BadRequestError('Username required.')};
     if (!dto.password)
         return {status: false, error: new BadRequestError('Password required.')};
+    if (await USERS_REPOSITORY.exists({username: dto.username}))
+        return {status: false, error: new ConflictError('Username taken.')};
     return {status: true};
 };
 
