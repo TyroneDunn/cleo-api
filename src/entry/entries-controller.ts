78c1b23d1a86f33c531254330755da87555ee57c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 17:07:21 +0200

    refactor(entries-controller): declare functions

--- src/entry/entries-controller.ts
+++ src/entry/entries-controller.ts
@@ -1,7 +1,19 @@
+import {Entry} from "./entry.type";
 import {EntriesRepository} from "./entries-repository";
 import {ENTRIES_REPOSITORY} from "../config";
+import {
+    CreateEntryDTO,
+    DeleteEntryDTO,
+    GetEntriesDTO,
+    GetEntryDTO,
+    UpdateEntryDTO
+} from "./entry-dtos";
 const repository: EntriesRepository = ENTRIES_REPOSITORY;
 
 export const EntriesController = {
-
+    getEntry: async (dto: GetEntryDTO): Promise<Entry> => {},
+    getEntries: async (dto: GetEntriesDTO): Promise<Entry> => {},
+    createEntry: async (dto: CreateEntryDTO): Promise<Entry> => {},
+    deleteEntry: async (dto: DeleteEntryDTO): Promise<Entry> => {},
+    updateEntry: async (dto: UpdateEntryDTO): Promise<Entry> => {},
 };
