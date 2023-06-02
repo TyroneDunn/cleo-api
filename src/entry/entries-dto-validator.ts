4774994f0ed6ceaa4ef783cd46020091296bb65a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-02 18:00:00 +0200

    feat(entries-dto-validator): implement 'validateGetEntryDTO'

--- src/entry/entries-dto-validator.ts
+++ src/entry/entries-dto-validator.ts
@@ -6,9 +6,19 @@ import {
     UpdateEntryDTO
 } from "./entries-dtos";
 import {ValidationResult} from "../utils/validation-result";
+import {BadRequestError, NotFoundError, UnauthorizedError} from "../utils/errors";
+import {ENTRIES_REPOSITORY} from "../config";
 
 export const validateGetEntryDTO = async (dto: GetEntryDTO): Promise<ValidationResult> => {
-
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!dto.id)
+        return {status: false, error: new BadRequestError('Entry ID required.')};
+    if (!(await ENTRIES_REPOSITORY.exists({id: dto.id})))
+        return {status: false, error: new NotFoundError(`Entry ${dto.id} not found.`)};
+    if (!(await ENTRIES_REPOSITORY.ownsEntry({userId: dto.userId, id: dto.id})))
+        return {status: false, error: new UnauthorizedError(`Unauthorized access to entry ${dto.id}`)};
+    return {status: true};
 };
 
 export const validateGetEntriesDTO = async (dto: GetEntriesDTO): Promise<ValidationResult> => {
