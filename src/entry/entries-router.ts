75419e02164ca516ae2941a79b19728c9e9baa0f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-03 16:07:13 +0200

    chore: clean

--- src/entry/entries-router.ts
+++ src/entry/entries-router.ts
@@ -10,67 +10,65 @@ import {
 import {User} from "../user/user.type";
 import {sendErrorResponse} from "../utils/send-error-response";
 
-const mapRequest = {
-    toGetEntryDTO: (req: Request): GetEntryDTO => ({
-        userId: (req.user as User)._id.toString(),
-        id: req.params.id,
-    }),
+const mapRequestToGetEntryDTO = (req: Request): GetEntryDTO => ({
+    userId: (req.user as User)._id.toString(),
+    id: req.params.id,
+});
 
-    toGetEntriesDTO: (req: Request): GetEntriesDTO => {
-        const dto: GetEntriesDTO = {userId: (req.user as User)._id.toString()}
-        if (req.query.id)
-            dto.journal = req.query.id as string;
-        if (req.query.body)
-            dto.body = req.query.body as string;
-        if (req.query.bodyRegex)
-            dto.bodyRegex = req.query.bodyRegex as string;
-        if (req.query.sort) {
-            if (req.query.sort === 'id')
-                dto.sort = 'id';
-            if (req.query.sort === 'body')
-                dto.sort = 'body';
-            if (req.query.sort === 'journal')
-                dto.sort = 'journal';
-            if (req.query.sort === 'dateCreated')
-                dto.sort = 'dateCreated';
-            if (req.query.sort === 'lastUpdated')
-                dto.sort = 'lastUpdated';
-        }
-        if (req.query.order)
-            req.query.order === '-1'? dto.order = -1: dto.order = 1;
-        if (req.query.page)
-            dto.page = parseInt(req.query.page as string);
-        if (req.query.limit)
-            dto.limit = parseInt(req.query.limit as string);
-        if (req.query.startDate)
-            dto.startDate = new Date(req.query.startdate as string)
-        if (req.query.endDate)
-            dto.endDate = new Date(req.query.enddate as string)
-        return dto;
-    },
-
-    toCreateEntryDTO: (req: Request): CreateEntryDTO => ({
-        userId: (req.user as User)._id.toString(),
-        journal: req.params.id,
-        body: req.body.body,
-    }),
-
-    toDeleteEntryDTO: (req: Request): DeleteEntryDTO => ({
-        userId: (req.user as User)._id.toString(),
-        id: req.params.id,
-    }),
-
-    toUpdateEntryDTO: (req: Request): UpdateEntryDTO => ({
-        userId: (req.user as User)._id.toString(),
-        id: req.params.id,
-        journal: req.body.journal,
-        body: req.body.body,
-    }),
+const mapRequestToGetEntriesDTO = (req: Request): GetEntriesDTO => {
+    const dto: GetEntriesDTO = {userId: (req.user as User)._id.toString()}
+    if (req.query.id)
+        dto.journal = req.query.id as string;
+    if (req.query.body)
+        dto.body = req.query.body as string;
+    if (req.query.bodyRegex)
+        dto.bodyRegex = req.query.bodyRegex as string;
+    if (req.query.sort) {
+        if (req.query.sort === 'id')
+            dto.sort = 'id';
+        if (req.query.sort === 'body')
+            dto.sort = 'body';
+        if (req.query.sort === 'journal')
+            dto.sort = 'journal';
+        if (req.query.sort === 'dateCreated')
+            dto.sort = 'dateCreated';
+        if (req.query.sort === 'lastUpdated')
+            dto.sort = 'lastUpdated';
+    }
+    if (req.query.order)
+        req.query.order === '-1'? dto.order = -1: dto.order = 1;
+    if (req.query.page)
+        dto.page = parseInt(req.query.page as string);
+    if (req.query.limit)
+        dto.limit = parseInt(req.query.limit as string);
+    if (req.query.startDate)
+        dto.startDate = new Date(req.query.startdate as string)
+    if (req.query.endDate)
+        dto.endDate = new Date(req.query.enddate as string)
+    return dto;
 };
+
+const mapRequestToCreateEntryDTO = (req: Request): CreateEntryDTO => ({
+    userId: (req.user as User)._id.toString(),
+    journal: req.params.id,
+    body: req.body.body,
+});
+
+const mapRequestToDeleteEntryDTO = (req: Request): DeleteEntryDTO => ({
+    userId: (req.user as User)._id.toString(),
+    id: req.params.id,
+});
+
+const mapRequestToUpdateEntryDTO = (req: Request): UpdateEntryDTO => ({
+    userId: (req.user as User)._id.toString(),
+    id: req.params.id,
+    journal: req.body.journal,
+    body: req.body.body,
+});
 
 const getEntry: RequestHandler = async (req: Request, res: Response) => {
     try {
-        const dto: GetEntryDTO = mapRequest.toGetEntryDTO(req);
+        const dto: GetEntryDTO = mapRequestToGetEntryDTO(req);
         res.json(await EntriesController.getEntry(dto));
     } catch (error) {
         sendErrorResponse(error, res);
@@ -79,7 +77,7 @@ const getEntry: RequestHandler = async (req: Request, res: Response) => {
 
 const getEntries: RequestHandler = async (req: Request, res: Response) => {
     try {
-        const dto: GetEntriesDTO = mapRequest.toGetEntriesDTO(req);
+        const dto: GetEntriesDTO = mapRequestToGetEntriesDTO(req);
         let entries = await EntriesController.getEntries(dto);
         res.json(entries);
     } catch (error) {
@@ -89,7 +87,7 @@ const getEntries: RequestHandler = async (req: Request, res: Response) => {
 
 const createEntry: RequestHandler = async (req: Request, res: Response) => {
     try {
-        const dto: CreateEntryDTO = mapRequest.toCreateEntryDTO(req);
+        const dto: CreateEntryDTO = mapRequestToCreateEntryDTO(req);
         res.json(await EntriesController.createEntry(dto));
     } catch (error) {
         sendErrorResponse(error, res);
@@ -98,7 +96,7 @@ const createEntry: RequestHandler = async (req: Request, res: Response) => {
 
 const deleteEntry: RequestHandler = async (req: Request, res: Response) => {
     try {
-        const dto: DeleteEntryDTO = mapRequest.toDeleteEntryDTO(req);
+        const dto: DeleteEntryDTO = mapRequestToDeleteEntryDTO(req);
         res.json(await EntriesController.deleteEntry(dto));
     } catch (error) {
         sendErrorResponse(error, res);
@@ -107,7 +105,7 @@ const deleteEntry: RequestHandler = async (req: Request, res: Response) => {
 
 const updateEntry: RequestHandler = async (req: Request, res: Response) => {
     try {
-        const dto: UpdateEntryDTO = mapRequest.toUpdateEntryDTO(req);
+        const dto: UpdateEntryDTO = mapRequestToUpdateEntryDTO(req);
         res.json(await EntriesController.updateEntry(dto));
     } catch (error) {
         sendErrorResponse(error, res);
