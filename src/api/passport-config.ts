4322b72467ecd70bc9d64e6a0c72c48aff881338
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-03 15:41:01 +0200

    refactor(passport-config): implement using repository

--- src/api/passport-config.ts
+++ src/api/passport-config.ts
@@ -1,28 +1,29 @@
 const passportConfig = require('passport');
 import LocalStrategy = require('passport-local');
 import {User} from "../user/user.type";
-import UserModel from "../user/mongo-user-model"
+import {USERS_REPOSITORY} from "../repositories-config";
 import {validatePassword} from "../utils/password-utils";
+import {QueryArgs} from "../user/users-repository";
 
 const userField = {
     usernameField: 'username',
     passwordField: 'password',
 };
 
-const verifyCallback = (username: string, password: string, done): void => {
-    UserModel.findOne({username: username}, (error, user: User) => {
-        if (!user) {
-            done(null, false);
-            return;
-        }
+const verifyCallback = async (username: string, password: string, done): Promise<void> => {
+    const args: QueryArgs = {username: username}
+    const user: User = await USERS_REPOSITORY.getUser(args);
+    if (!user) {
+        done(null, false);
+        return;
+    }
 
-        if (!validatePassword(password, user.hash)) {
-            done(null, false);
-            return;
-        }
+    if (!validatePassword(password, user.hash)) {
+        done(null, false);
+        return;
+    }
 
-        done(null, user);
-    });
+    done(null, user);
 };
 
 const localStrategy = new LocalStrategy.Strategy(userField, verifyCallback);
@@ -32,12 +33,12 @@ passportConfig.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passportConfig.deserializeUser((userId, done) => {
-    UserModel.findById(userId, (error, user) => {
-       if (error) {
-           done(error);
-           return;
-       }
+passportConfig.deserializeUser(async (userId, done) => {
+    try {
+        const args: QueryArgs = {id: userId};
+        const user = await USERS_REPOSITORY.getUser(args);
         done(null, user);
-    });
+    } catch (error) {
+        done(error);
+    }
 });
