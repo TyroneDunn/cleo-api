68141e63e47570735a6d6ec40e9e710d89034941
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-03 12:42:31 +0200

    feat(users-repository): add query arg options

--- src/user/users-repository.ts
+++ src/user/users-repository.ts
@@ -2,13 +2,36 @@ import {User} from "./user.type";
 
 export type QueryArgs = {
     id?: string,
+    idRegex?: string,
     username?: string,
+    usernameRegex?: string,
     hash?: string,
+    hashRegex?: string,
+};
+
+export type SortArgs = {
+    sort?: 'id' | 'username' | 'hash',
+    order?: 1 | -1,
+};
+
+export type FilterArgs = {
+    startDate?: Date,
+    endDate?: Date,
+};
+
+export type PaginationArgs = {
+    page?: number,
+    limit?: number,
 };
 
 export type UsersRepository = {
     getUser: (args: QueryArgs) => Promise<User>,
-    getUsers: (args: QueryArgs) => Promise<User[]>,
+    getUsers: (
+        queryArgs: QueryArgs,
+        sortArgs: SortArgs,
+        filterArgs: FilterArgs,
+        paginationArgs: PaginationArgs
+    ) => Promise<User[]>,
     registerUser: (args: QueryArgs) => Promise<User>,
     deleteUser: (args: QueryArgs) => Promise<User>,
     updateUser: (args: QueryArgs) => Promise<User>,
