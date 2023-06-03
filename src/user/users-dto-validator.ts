f446303c07d04689328285301ff1566232aa6d5f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-03 12:33:24 +0200

    feat(users-dto-validator): implement validation functions

--- src/user/users-dto-validator.ts
+++ src/user/users-dto-validator.ts
@@ -1,6 +1,6 @@
-import {ExistsDTO, RegisterUserDTO} from "./users-dtos";
+import {DeleteUserDTO, GetUserDTO, GetUsersDTO, RegisterUserDTO} from "./users-dtos";
 import {ValidationResult} from "../utils/validation-result";
-import {BadRequestError, ConflictError} from "../utils/errors";
+import {BadRequestError, ConflictError, ForbiddenError, NotFoundError} from "../utils/errors";
 import {USERS_REPOSITORY} from "../config";
 
 export const validateRegisterUserDTO = async (dto: RegisterUserDTO): Promise<ValidationResult> => {
@@ -13,8 +13,37 @@ export const validateRegisterUserDTO = async (dto: RegisterUserDTO): Promise<Val
     return {status: true};
 };
 
-export const validateExistsDTO = async (dto: ExistsDTO): Promise<ValidationResult> => {
-    if (!dto.username)
-        return {status: false, error: new BadRequestError('Username required.')};
+export const validateGetUserDTO = async (dto: GetUserDTO) => {
+    if (!(await USERS_REPOSITORY.isPrivileged({id: dto.userId})))
+        return {status: false, error: new ForbiddenError('Unauthorized.')};
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('Request user ID required.')};
+    if (!dto.id)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!(await USERS_REPOSITORY.exists({username: dto.id})))
+        return {status: false, error: new NotFoundError('User not found.')};
+    return {status: true};
+};
+
+export const validateGetUsersDTO = async (dto: GetUsersDTO) => {
+    if (!(await USERS_REPOSITORY.isPrivileged({id: dto.userId})))
+        return {status: false, error: new ForbiddenError('Unauthorized.')};
+    if ((dto.id && dto.idRegex) ||
+        (dto.username && dto.usernameRegex)) {
+        return {status: false, error: new BadRequestError('Invalid query.')};
+    }
+    return {status: true};
+};
+
+export const validateDeleteUserDTO = async (dto: DeleteUserDTO) => {
+    if (!(await USERS_REPOSITORY.isPrivileged({id: dto.userId})))
+        return {status: false, error: new ForbiddenError('Unauthorized.')};
+    if (!dto.userId)
+        return {status: false, error: new BadRequestError('User ID required.')};
+    if (!dto.id)
+        return {status: false, error: new BadRequestError('Journal ID required.')};
+    if (!(await USERS_REPOSITORY.exists({id: dto.id}))) {
+        return {status: false, error: new NotFoundError(`User ${dto.id} not found.`)};
+    }
     return {status: true};
 };
