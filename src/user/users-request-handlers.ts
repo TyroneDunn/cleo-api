c3c1ca14986c50e27cbe642fdc5d17e2f1e429d5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-04 17:15:28 +0200

    refactor(users-request-handlers): extract request handlers

---
+++ src/user/users-request-handlers.ts
@@ -0,0 +1,76 @@
+import {Request, RequestHandler, Response} from "express";
+import {DeleteUserDTO, GetUserDTO, GetUsersDTO, UpdateUserDTO} from "./users-dtos";
+import {User} from "./user";
+import {UsersController} from "./users-controller";
+import {sendErrorResponse} from "../utils/send-error-response";
+
+const mapToGetUsersDTO = (req: Request): GetUsersDTO => {
+    return {
+        senderId: (req.user as User)._id.toString(),
+        ...req.query.idRegex && {id: req.query.idRegex as string},
+        ...req.query.username && {username: req.query.username as string},
+        ...req.query.usernameRegex && {usernameRegex: req.query.usernameRegex as string},
+        ...req.query.sort && {sort: req.query.sort as "username" | "id" | "dateCreated" | "lastUpdated"},
+        ...req.query.order && {order: parseInt(req.query.order as string) as 1 | -1},
+        ...req.query.page && {page: parseInt(req.query.page as string)},
+        ...req.query.limit && {limit: parseInt(req.query.page as string)},
+        ...req.query.startDate && {startDate: new Date(req.query.startDate as string)},
+        ...req.query.endDate && {endDate: new Date(req.query.endDate as string)},
+    };
+};
+
+const mapToGetUserDTO = (req: Request) => ({
+    senderId: (req.user as User)._id.toString(),
+    id: req.params.id,
+});
+
+const mapToUpdateUserDTO = (req: Request) => {
+    return {
+        senderId: (req.user as User)._id.toString(),
+        id: req.params.id,
+        ...req.body.username && {username: req.body.username},
+        ...req.body.password && {password: req.body.password},
+    }
+};
+
+const mapToDeleteUserDTO = (req: Request) => ({
+    senderId: (req.user as User)._id.toString(),
+    id: req.params.id,
+});
+
+export const getUser: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const dto: GetUserDTO = mapToGetUserDTO(req);
+        const user: User = await UsersController.getUser(req.user as User, dto);
+        res.json(user);
+    } catch (error) {
+        sendErrorResponse(error, res);
+    }
+};
+export const getUsers: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const dto: GetUsersDTO = mapToGetUsersDTO(req);
+        const users: User[] = await UsersController.getUsers(req.user as User, dto);
+        res.json(users);
+    } catch (error) {
+        sendErrorResponse(error, res);
+    }
+};
+export const deleteUser: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const dto: DeleteUserDTO = mapToDeleteUserDTO(req);
+        const user: User = await UsersController.deleteUser(req.user as User, dto);
+        res.json(user);
+    } catch (error) {
+        sendErrorResponse(error, res);
+    }
+};
+export const updateUser: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const dto: UpdateUserDTO = mapToUpdateUserDTO(req);
+        const user: User = await UsersController.updateUser(req.user as User, dto);
+        res.json(user);
+    } catch (error) {
+        sendErrorResponse(error, res);
+    }
+};
