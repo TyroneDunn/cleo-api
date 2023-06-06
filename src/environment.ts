5a8445ee553f7639400b46d538e448a65cb88777
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-06-06 10:22:19 +0200

    chore: clean

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 export const NODE_ENV: string = process.env.NODE_ENV;
 export const API_TITLE: string = process.env.API_TITLE;
 export const API_PORT: number = parseInt(process.env.API_PORT);
-export const APP_URL = process.env.APP_URL;
+export const APP_URL: string = process.env.APP_URL;
 export const MONGODB_URL: string = process.env.MONGO_DB_URL;
 export const SESSION_SECRET: string = process.env.SESSION_SECRET;
 export const PASSWORD_SALT: string = process.env.PASSWORD_SALT;
