8e985612bec727316dfdc4fb34f8c6c169e160c9
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-10-21 18:35:32 +0200

    feat: implement query journals by id and author

--- src/journal/journals-request-handlers.ts
+++ src/journal/journals-request-handlers.ts
@@ -70,14 +70,17 @@ const mapToGetJournalDTO = (req: Request): GetJournalDTO =>
     ({id: req.params.id});
 
 const mapToGetJournalsDTO = (req: Request): GetJournalsDTO => ({
+    ... req.query.idRegex && {idRegex: req.query.idRegex as string},
     ... req.query.name && {name: req.query.name as string},
     ... req.query.nameRegex && {nameRegex: req.query.nameRegex as string},
+    ... req.query.author && {author: req.query.author as string},
+    ... req.query.authorRegex && {authorRegex: req.query.authorRegex as string},
+    ... req.query.startDate && {startDate: req.query.startDate as string},
+    ... req.query.endDate && {endDate: req.query.endDate as string},
     ... req.query.sort && {sort: req.query.sort as "name" | "author" | "id" | "dateCreated" | "lastUpdated"},
     ... req.query.order && {order: parseInt(req.query.order as string) as 1 | -1},
     ... req.query.page && {page: parseInt(req.query.page as string)},
     ... req.query.limit && {limit: parseInt(req.query.page as string)},
-    ... req.query.startDate && {startDate: new Date(req.query.startDate as string)},
-    ... req.query.endDate && {endDate: new Date(req.query.endDate as string)},
 });
 
 const mapToCreateJournalDTO = (req: Request): CreateJournalDTO => ({
