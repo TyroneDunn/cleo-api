df2333212f997236546da9b14e151309bd528742
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-10-22 15:36:39 +0200

    feat: define delete entries API endpoint

--- src/entry/entries-router.ts
+++ src/entry/entries-router.ts
@@ -11,6 +11,7 @@ const entriesRouter: Router = Router();
 entriesRouter.get('/:id', getEntryHandler);
 entriesRouter.get('', getEntriesHandler);
 entriesRouter.post('/:id', createEntryHandler);
+entriesRouter.delete('/', deleteEntriesHandler);
 entriesRouter.delete('/:id', deleteEntryHandler);
 entriesRouter.patch('/:id', updateEntryHandler);
 
