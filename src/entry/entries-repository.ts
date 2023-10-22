ec73535ac748e87ee5e7f4753a87f6d969ab564f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-10-22 15:46:24 +0200

    feat: define delete entries function

--- src/entry/entries-repository.ts
+++ src/entry/entries-repository.ts
@@ -1,18 +1,20 @@
 import {Entry} from "./entry";
 import {
-    CreateEntryDTO,
-    DeleteEntryDTO,
     GetEntriesDTO,
     GetEntryDTO,
-    UpdateEntryDTO
+    CreateEntryDTO,
+    UpdateEntryDTO,
+    DeleteEntryDTO,
+    DeleteEntriesDTO,
 } from "./entries-dtos";
 
 export type EntriesRepository = {
     getEntry: (dto: GetEntryDTO) => Promise<Entry>,
     getEntries: (dto: GetEntriesDTO) => Promise<Entry[]>,
     createEntry: (dto: CreateEntryDTO) => Promise<Entry>,
-    deleteEntry: (dto: DeleteEntryDTO) => Promise<Entry>,
     updateEntry: (dto: UpdateEntryDTO) => Promise<Entry>,
+    deleteEntry: (dto: DeleteEntryDTO) => Promise<Entry>,
+    deleteEntries: (dto: DeleteEntriesDTO) => Promise<string>,
     exists: (id: string) => Promise<boolean>,
     ownsEntry: (author: string, id: string) => Promise<boolean>,
 };
