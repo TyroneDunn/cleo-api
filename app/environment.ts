55e35669d85cfa43992579f6c3fd04d3064636f3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-01 21:20:15 +0200

    feat: add additional environment values

--- app/environment.ts
+++ app/environment.ts
@@ -1,9 +1,12 @@
 require('dotenv').config();
 export const NODE_ENV: string = process.env.NODE_ENV;
 export const API_TITLE: string = process.env.API_TITLE;
+export const API_VERSION: string = process.env.API_VERSION;
 export const API_PORT: number = parseInt(process.env.API_PORT);
 export const APP_URL: string = process.env.APP_URL;
 export const MONGODB_URL: string = process.env.MONGO_DB_URL;
+export const USERS_DB_NAME: string = process.env.USERS_DB_NAME || '';
+export const USERS_DB_OPTION: string = process.env.USERS_DB_OPTION || '';
 export const SESSION_SECRET: string = process.env.SESSION_SECRET;
 export const PASSWORD_SALT: string = process.env.PASSWORD_SALT;
 export const PASSWORD_LENGTH: number = parseInt(process.env.PASSWORD_LENGTH);
