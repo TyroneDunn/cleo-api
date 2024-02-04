74caa14269545e72060bf6d7f46b3279aa12362c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-04 20:53:30 +0200

    chore: format

--- app/entries/mongo-entry-model.ts
+++ app/entries/mongo-entry-model.ts
@@ -1,30 +1,30 @@
-import {Entry} from "./entries.types";
-import {Document, Schema} from 'mongoose';
+import { Entry } from "./entries.types";
+import { Document, Schema } from 'mongoose';
 import database from "../database/mongoose-database";
 
 interface EntryDocument extends Document, Entry {
-    _id: string,
-    body: string,
-    title: string,
-    journal: Schema.Types.ObjectId;
+   _id     : string,
+   body    : string,
+   title   : string,
+   journal : Schema.Types.ObjectId;
 }
 
 const EntrySchema = new Schema({
-    body: {
-        type: String,
-    },
-    title: {
-        type: String,
-    },
-    journal: {
-        type: Schema.Types.ObjectId,
-        ref: "JournalSchema",
-        required: true,
-    },
-},
-{
-    timestamps: true,
-});
+      body : {
+         type : String,
+      },
+      title : {
+         type : String,
+      },
+      journal : {
+         type     : Schema.Types.ObjectId,
+         ref      : "JournalSchema",
+         required : true,
+      },
+   },
+   {
+      timestamps : true,
+   });
 
 const EntryModel = database.model<EntryDocument>('Entry', EntrySchema);
 export default EntryModel;
