f3e4477c1b362b6dda6563512a955c7d0d186000
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-04 20:52:38 +0200

    refactor: simplify timestamps

--- app/journals/mongo-journal-model.type.ts
+++ app/journals/mongo-journal-model.type.ts
@@ -1,35 +1,34 @@
-import {Journal} from "./journals.types";
-import {Document, Schema} from 'mongoose';
+import { Journal } from "./journals.types";
+import { Document, Schema } from 'mongoose';
 import database from '../database/mongoose-database';
 
 interface JournalDocument extends Document, Journal {
-  _id: string,
-  name: string,
-  author: string;
-  createdAt: Date,
-  updatedAt: Date,
+   _id    : string,
+   name   : string,
+   author : string;
 }
 
-const journalSchema = new Schema<JournalDocument>({
-  name: {
-    type: String,
-    required: true
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-},
-{
-  timestamps: true,
-  toObject: {virtuals: true}
-});
+const journalSchema = new Schema<JournalDocument>(
+   {
+      name  : {
+         type     : String,
+         required : true,
+      },
+      author: {
+         type     : String,
+         required : true,
+      },
+   },
+   {
+      timestamps : true,
+      toObject   : { virtuals: true },
+   });
 
 journalSchema.virtual('journals-author', {
-  ref: 'User',
-  localField: 'author',
-  foreignField: 'username',
-  justOne: true
+   ref          : 'User',
+   localField   : 'author',
+   foreignField : 'username',
+   justOne      : true,
 });
 
 const JournalModel = database.model<JournalDocument>('Journal', journalSchema);
