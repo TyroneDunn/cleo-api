40609e0717910ef6e1a91a3b9a3c29d94324fd7f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-05 14:50:57 +0200

    refactor: improve validator implementation, invert dependencies

--- app/entries/entries.validator.ts
+++ app/entries/entries.validator.ts
@@ -1,16 +1,3 @@
-import {ValidationResult} from "../utils/validation-result";
-import {
-    BadRequestError,
-    ForbiddenError,
-    NotFoundError,
-    UnauthorizedError
-} from "../utils/errors";
-import {
-    ENTRIES_REPOSITORY,
-    JOURNALS_REPOSITORY,
-    USERS_REPOSITORY
-} from "../repositories-config";
-import {User} from "../user/user";
 import {UsersRepository} from "../user/users-repository";
 import {JournalsRepository} from "../journals/journals-repository.type";
 import {EntriesRepository} from "./entries-repository.type";
@@ -21,119 +8,185 @@ import {
     GetEntriesRequest, GetEntryRequest,
     UpdateEntryRequest,
 } from './entries.types';
+import { ValidationError } from '@hals/common';
 
-const usersRepository: UsersRepository = USERS_REPOSITORY;
-const journalsRepository: JournalsRepository = JOURNALS_REPOSITORY;
-const entriesRepository: EntriesRepository = ENTRIES_REPOSITORY;
-
-export const validateGetEntryDTO = async (user: User, dto: GetEntryRequest): Promise<ValidationResult> => {
-    if (!(user))
-        return {error: new UnauthorizedError('Unauthorized.')};
-    if (!dto.id)
-        return {error: new BadRequestError('Entry ID required.')};
-    if (!(await entriesRepository.exists(dto.id)))
-        return {error: new NotFoundError(`Entry ${dto.id} not found.`)};
-    if (!(await usersRepository.isAdmin(user.username)) && !(await entriesRepository.ownsEntry(user.username, dto.id)))
-        return {error: new ForbiddenError('Insufficient permissions.')};
-    return {};
+export type EntriesValidator = {
+    validateGetEntryRequest : (request : GetEntryRequest) => Promise<ValidationError | null>,
+    validateGetEntriesRequest : (request : GetEntriesRequest) => Promise<ValidationError | null>,
+    validateCreateEntryRequest : (request : CreateEntryRequest) => Promise<ValidationError | null>,
+    validateUpdateEntryRequest : (request : UpdateEntryRequest) => Promise<ValidationError | null>,
+    validateDeleteEntryRequest : (request : DeleteEntryRequest) => Promise<ValidationError | null>,
+    validateDeleteEntriesRequest : (request : DeleteEntriesRequest) => Promise<ValidationError | null>,
 };
 
-export const validateGetEntriesDTO = async (user: User, dto: GetEntriesRequest): Promise<ValidationResult> => {
-    if (!(user))
-        return {error: new UnauthorizedError('Unauthorized.')};
-    if (dto.journal)
-        if (!(await journalsRepository.exists(dto.journal)))
-            return {error: new NotFoundError(`Journal ${dto.journal} not found.`)};
-    if (!(await usersRepository.isAdmin(user.username)) && !(await journalsRepository.ownsJournal(user.username, dto.journal)))
-        return {error: new ForbiddenError('Insufficient permissions.')};
-    if ((dto.title && dto.titleRegex))
-        return {error: new BadRequestError('Invalid query. Provide either "title" or' +
-                ' "titleRegex".')};
-    if ((dto.body && dto.bodyRegex))
-        return {error: new BadRequestError('Invalid query. Provide either "body" or "bodyRegex".')};
-    if (dto.startDate)
-        if (isNaN(Date.parse(dto.startDate)))
-            return {error: new BadRequestError('Invalid start date query. Provide a ISO date string.')};
-    if (dto.endDate)
-        if (isNaN(Date.parse(dto.endDate)))
-            return {error: new BadRequestError('Invalid end date query. Provide a ISO date string.')};
-    if (dto.sort)
-        if (dto.sort !== 'id' && dto.sort !== 'title'&& dto.sort !== 'body' && dto.sort !== 'journal' && dto.sort !== 'lastUpdated' && dto.sort !== 'dateCreated')
-            return {error: new BadRequestError('Invalid query. Sort option must' +
-                    ' be id, title, body, journals, lastUpdated, or dateCreated.')};
-    if (dto.order !== undefined)
-        if ((dto.order !== 'asc' && dto.order !== 'desc'))
-            return {error: new BadRequestError('Invalid query. Order must be' +
-                    ' "asc" or "desc".')};
-    if (dto.page !== undefined)
-        if (dto.page < 0)
-            return {error: new BadRequestError('Invalid query. Page must be' +
-                    ' 0 or greater.')};
-    if (dto.limit !== undefined)
-        if (dto.limit < 0)
-            return {error: new BadRequestError('Invalid query. Limit must be' +
-                    ' 0 or greater.')};
-    return {};
-};
+export const EntriesValidator = (
+   usersRepository: UsersRepository,
+   journalsRepository: JournalsRepository,
+   entriesRepository: EntriesRepository,
+) : EntriesValidator => ({
+    validateGetEntryRequest : async (request : GetEntryRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (!request.id)
+            return ValidationError('BadRequest', 'Entry ID required.');
+        if (!(await entriesRepository.exists(request.id)))
+            return ValidationError('NotFound', `Entry ${request.id} not found.`);
+        if (!(await usersRepository.isAdmin(request.user.username)) && !(await entriesRepository.ownsEntry(request.user.username, request.id)))
+            return ValidationError('Forbidden', 'Insufficient permissions.');
+        return null;
+    },
 
-export const validateCreateEntryDTO = async (user: User, dto: CreateEntryRequest): Promise<ValidationResult> => {
-    if (!user)
-        return {error: new UnauthorizedError('Unauthorized.')};
-    if (!dto.journal)
-        return {error: new BadRequestError('JournalsTypes required.')};
-    if (!(await journalsRepository.exists(dto.journal)))
-        return {error: new NotFoundError(`Journal ${dto.journal} not found.`)};
-    if (!(await usersRepository.isAdmin(user.username)) && !(await journalsRepository.ownsJournal(user.username, dto.journal)))
-        return {error: new ForbiddenError('Insufficient permissions.')};
-    return {};
-};
+    validateGetEntriesRequest : async (request : GetEntriesRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (request.filter) {
+            if (request.filter.journal) {
+                if (!(await journalsRepository.exists(request.filter.journal)))
+                    return ValidationError('NotFound', `Journal ${request.filter.journal} not found.`);
+                if (!(await usersRepository.isAdmin(request.user.username)) && !(await journalsRepository.ownsJournal(request.user.username, request.filter.journal)))
+                    return ValidationError('Forbidden', 'Insufficient permissions.');
+            }
+            if ((request.filter.title && request.filter.titleRegex))
+                return ValidationError('BadRequest', 'Invalid query. Provide either "title" or' +
+                   ' "titleRegex".')
+            if ((request.filter.body && request.filter.bodyRegex))
+                return ValidationError('BadRequest', 'Invalid query. Provide either "body" or "bodyRegex".')
+            if (request.filter.timestamps) {
+                if (request.filter.timestamps.createdAt) {
+                    if (request.filter.timestamps.createdAt.start) {
+                        if (isNaN(Date.parse(request.filter.timestamps.createdAt.start)))
+                            return ValidationError('BadRequest', 'Invalid created at start date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                    if (request.filter.timestamps.createdAt.end) {
+                        if (isNaN(Date.parse(request.filter.timestamps.createdAt.end)))
+                            return ValidationError('BadRequest', 'Invalid created at end date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                }
+                if (request.filter.timestamps.updatedAt) {
+                    if (request.filter.timestamps.updatedAt.start) {
+                        if (isNaN(Date.parse(request.filter.timestamps.updatedAt.start)))
+                            return ValidationError('BadRequest', 'Invalid updated at start date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                    if (request.filter.timestamps.updatedAt.end) {
+                        if (isNaN(Date.parse(request.filter.timestamps.updatedAt.end)))
+                            return ValidationError('BadRequest', 'Invalid updated at end date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                }
+            }
+        }
+        if (request.sort) {
+            if (request.sort.sortBy && !request.sort.order)
+                return ValidationError("BadRequest", 'Invalid sort query. Provide sort order.');
+            if (!request.sort.sortBy && request.sort.order)
+                return ValidationError("BadRequest", 'Invalid sort query. Provide sort by field.');
+            if (request.sort.sortBy !== 'id'
+               && request.sort.sortBy !== 'title'
+               && request.sort.sortBy !== 'body'
+               && request.sort.sortBy !== 'journal'
+               && request.sort.sortBy !== 'updatedAt'
+               && request.sort.sortBy !== 'createdAt')
+                return ValidationError('BadRequest', 'Invalid query. Sort option must' +
+                   ' be id, title, body, journal, updatedAt, or createdAt.');
+            if ((request.sort.order !== 'asc' && request.sort.order !== 'desc'))
+                return ValidationError('BadRequest', 'Invalid sort query. Order must be "asc" or "desc".');
+        }
+        if (request.page === undefined)
+            return ValidationError('BadRequest', 'Invalid query. Page index and limit required.');
+        if (request.page.index < 0)
+            return ValidationError('BadRequest', 'Invalid query. Page index must be 0 or greater.');
+        if (request.page.limit < 1)
+            return ValidationError('BadRequest', 'Invalid query. Page limit must be greater than 0.');
+        return null;
+    },
 
-export const validateUpdateEntryDTO = async (user: User, dto: UpdateEntryRequest): Promise<ValidationResult> => {
-    if (!user)
-        return {error: new UnauthorizedError('Unauthorized.')};
-    if (!dto.id)
-        return {error: new BadRequestError('Entry ID required.')};
-    if (!(await usersRepository.isAdmin(user.username)) && !(await entriesRepository.ownsEntry(user.username, dto.id)))
-        return {error: new ForbiddenError('Insufficient permissions.')};
-    if (!(await entriesRepository.exists(dto.id)))
-        return {error: new NotFoundError(`Entry ${dto.id} not found.`)};
-    if ((!dto.body) && (!dto.journal) && (!dto.title))
-        return {error: new BadRequestError('Update field required.')};
-    if (dto.journal) {
-        if (!(await journalsRepository.exists(dto.journal)))
-            return {error: new NotFoundError(`Journal ${dto.journal} not found.`)};
-    }
-    return {};
-};
+    validateCreateEntryRequest : async (request : CreateEntryRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (!request.journal)
+            return ValidationError('BadRequest', 'Journal required.');
+        if (!(await journalsRepository.exists(request.journal)))
+            return ValidationError('NotFound', `Journal ${request.journal} not found.`);
+        if (!(await usersRepository.isAdmin(request.user.username)) && !(await journalsRepository.ownsJournal(request.user.username, request.journal)))
+            return ValidationError('Forbidden', 'Insufficient permissions.');
+        return null;
+    },
 
-export const validateDeleteEntryDTO = async (user: User, dto: DeleteEntryRequest): Promise<ValidationResult> => {
-    if (!dto.id)
-        return {error: new BadRequestError('Entry ID required.')};
-    if (!(await ENTRIES_REPOSITORY.exists(dto.id)))
-        return {error: new NotFoundError(`Entry ${dto.id} not found.`)};
-    if (!(await ENTRIES_REPOSITORY.ownsEntry(user.username, dto.id)))
-        return {error: new UnauthorizedError(`Unauthorized access to entry ${dto.id}`)};
-    return {};
-};
+    validateUpdateEntryRequest : async (request : UpdateEntryRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (!request.id)
+            return ValidationError('BadRequest', 'Entry ID required.');
+        if (!(await usersRepository.isAdmin(request.user.username)) && !(await entriesRepository.ownsEntry(request.user.username, request.id)))
+            return ValidationError('Forbidden', 'Insufficient permissions.');
+        if (!(await entriesRepository.exists(request.id)))
+            return ValidationError('NotFound', `Entry ${request.id} not found.`);
+        if ((!request.body) && (!request.journal) && (!request.title))
+            return ValidationError('BadRequest', 'Entry update field required.');
+        if (request.journal) {
+            if (!(await journalsRepository.exists(request.journal)))
+                return ValidationError('NotFound', `Journal ${request.journal} not found.`);
+        }
+        return null;
+    },
 
-export const validateDeleteEntriesDTO = async (user: User, dto: DeleteEntriesRequest): Promise<ValidationResult> => {
-    if (!user)
-        return {error: new UnauthorizedError('Unauthorized.')};
-    if (dto.journal)
-        if (!(await journalsRepository.exists(dto.journal)))
-            return {error: new NotFoundError(`Journal ${dto.journal} not found.`)};
-    if (!(await usersRepository.isAdmin(user.username)) && !(await journalsRepository.ownsJournal(user.username, dto.journal)))
-        return {error: new ForbiddenError('Insufficient permissions.')};
-    if ((dto.title && dto.titleRegex))
-        return {error: new BadRequestError('Invalid query. Provide either "title" or' +
-                ' "titleRegex".')};
-    if ((dto.body && dto.bodyRegex))
-        return {error: new BadRequestError('Invalid query. Provide either "body" or "bodyRegex".')};
-    if (dto.startDate)
-        if (isNaN(Date.parse(dto.startDate)))
-            return {error: new BadRequestError('Invalid start date query. Provide a ISO date string.')};
-    if (dto.endDate)
-        if (isNaN(Date.parse(dto.endDate)))
-            return {error: new BadRequestError('Invalid end date query. Provide a ISO date string.')};
-    return {};
-};
+    validateDeleteEntryRequest : async (request : DeleteEntryRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (!request.id)
+            return ValidationError('BadRequest', 'Entry ID required.');
+        if (!(await entriesRepository.exists(request.id)))
+            return ValidationError('NotFound', `Entry ${request.id} not found.`);
+        if (!(await usersRepository.isAdmin(request.user.username)) && !(await entriesRepository.ownsEntry(request.user.username, request.id)))
+            return ValidationError('Forbidden', 'Insufficient permissions.');
+        return null;
+    },
+
+    validateDeleteEntriesRequest : async (request : DeleteEntriesRequest) : Promise<ValidationError | null> => {
+        if (!request.user)
+            return ValidationError('Unauthorized', 'Unauthorized user.');
+        if (request.filter) {
+            if (request.filter.journal) {
+                if (!(await journalsRepository.exists(request.filter.journal)))
+                    return ValidationError('NotFound', `Journal ${request.filter.journal} not found.`);
+                if (!(await usersRepository.isAdmin(request.user.username)) && !(await journalsRepository.ownsJournal(request.user.username, request.filter.journal)))
+                    return ValidationError('Forbidden', 'Insufficient permissions.');
+            }
+            if ((request.filter.title && request.filter.titleRegex))
+                return ValidationError('BadRequest', 'Invalid query. Provide either "title" or' +
+                   ' "titleRegex".')
+            if ((request.filter.body && request.filter.bodyRegex))
+                return ValidationError('BadRequest', 'Invalid query. Provide either "body" or "bodyRegex".')
+            if (request.filter.timestamps) {
+                if (request.filter.timestamps.createdAt) {
+                    if (request.filter.timestamps.createdAt.start) {
+                        if (isNaN(Date.parse(request.filter.timestamps.createdAt.start)))
+                            return ValidationError('BadRequest', 'Invalid created at start date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                    if (request.filter.timestamps.createdAt.end) {
+                        if (isNaN(Date.parse(request.filter.timestamps.createdAt.end)))
+                            return ValidationError('BadRequest', 'Invalid created at end date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                }
+                if (request.filter.timestamps.updatedAt) {
+                    if (request.filter.timestamps.updatedAt.start) {
+                        if (isNaN(Date.parse(request.filter.timestamps.updatedAt.start)))
+                            return ValidationError('BadRequest', 'Invalid updated at start date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                    if (request.filter.timestamps.updatedAt.end) {
+                        if (isNaN(Date.parse(request.filter.timestamps.updatedAt.end)))
+                            return ValidationError('BadRequest', 'Invalid updated at end date query.' +
+                               ' Provide a ISO date string.');
+                    }
+                }
+            }
+        }
+        return null;
+    },
+});
