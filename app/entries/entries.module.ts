d10d155144c56d42291d4fdbbfca6fa10e1105f5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-05 13:29:40 +0200

    feat: define entries module

---
+++ app/entries/entries.module.ts
@@ -0,0 +1,18 @@
+import { EntriesController } from './entries.controller';
+import { Controller } from '@hals/common';
+import { EntriesService } from './entries.service';
+import { MongoEntriesRepository } from './mongo-entries-repository';
+import { EntriesValidator } from './entries.validator';
+import { MongoUsersRepository } from '../user/mongo-users-repository';
+import { MongoJournalsRepository } from '../journals/mongo-journals-repository.service';
+
+const entriesService : EntriesService = EntriesService(
+   MongoEntriesRepository,
+   EntriesValidator(
+      MongoUsersRepository,
+      MongoJournalsRepository,
+      MongoEntriesRepository
+   ));
+const entriesController : Controller = EntriesController(entriesService);
+
+export default entriesController;
