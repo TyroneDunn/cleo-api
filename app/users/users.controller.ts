1f38cc867b9527632c0cfe71c5aa446326c7099b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-06 22:33:00 +0200

    feat: define users controller

---
+++ app/users/users.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Method, RequestHandler } from '@hals/common';
+import { UsersService } from './users.service';
+import { logGetRequest, logPatchRequest } from '../logs/log.utility';
+
+export const UsersController = (service : UsersService) : Controller => ({
+   path    : 'users/',
+   guard   : true,
+   methods : [
+      getUsersMethod(service.getUsers),
+      getUserMethod(service.getUser),
+      updateUserMethod(service.updateUser),
+   ],
+});
+
+const getUsersMethod = (getUsers : RequestHandler) : Method => ({
+   type           : "GET",
+   queryParamKeys : UsersQueryParamKeys,
+   sideEffects    : [ logGetRequest("Get Users") ],
+   requestHandler : getUsers,
+});
+
+const getUserMethod = (getUser : RequestHandler) : Method => ({
+   type           : "GET",
+   paramKeys      : [ ID ],
+   sideEffects    : [ logGetRequest("Get User") ],
+   requestHandler : getUser,
+});
+
+const updateUserMethod = (updateUser : RequestHandler) : Method => ({
+   type           : "PATCH",
+   paramKeys      : [ ID ],
+   sideEffects    : [ logPatchRequest("Patch User") ],
+   requestHandler : updateUser,
+});
+
+const ID : string = 'id';
+
+const UsersQueryParamKeys : string[] = [
+   'username',
+   'id',
+   'createdAt',
+   'updatedAt',
+   'sortBy',
+   'order',
+   'index',
+   'limit',
+];
