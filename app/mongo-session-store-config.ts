461dacfcd4c46aefbdcd9b1f9c4d8415c5868eb0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-07 18:49:55 +0200

    refactor: remove express app auth implementations

--- app/mongo-session-store-config.ts
+++
@@ -1,7 +0,0 @@
-import MongoStore = require('connect-mongo');
-import {MONGODB_URL} from "./environment";
-
-export const mongoSessionStore: MongoStore = MongoStore.create({
-    mongoUrl: MONGODB_URL,
-    collectionName: 'sessions',
-});
