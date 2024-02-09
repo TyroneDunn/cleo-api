17aef1fd11f3e53bb8fb6a340d93dda710d640b5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-09 14:16:02 +0200

    feat: define create metadata function

--- app/users/users-metadata-repository.type.ts
+++ app/users/users-metadata-repository.type.ts
@@ -1,8 +1,9 @@
 import { CommandResult, Error } from '@hals/common';
-import { UpdateUserRequest, UserMetadata } from './users.types';
+import { CreateUserMetadataRequest, UpdateUserRequest, UserMetadata } from './users.types';
 
 export type UsersMetadataRepository = {
    getUserMetadata: (username: string) => Promise<UserMetadata | Error>,
+   createUserMetadata: (request : CreateUserMetadataRequest) => Promise<UserMetadata | Error>,
    updateUserMetadata: (request : UpdateUserRequest) => Promise<CommandResult | Error>,
    deleteUserMetadata: (username: string) => Promise<CommandResult | Error>,
    isAdmin: (username: string) => Promise<boolean | Error>,
