d1fe3f58ff44b80b0b72bb447c13432d4096d37a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-09 14:20:04 +0200

    refactor: update journals validator implementation

--- app/journals/journals.module.ts
+++ app/journals/journals.module.ts
@@ -1,13 +1,13 @@
 import { JournalsService } from './journals.service';
 import { MongoJournalsRepository } from './mongo-journals-repository.service';
-import { MongoUsersRepository } from '../users/mongo-users-repository.service';
 import { JournalsValidator } from './journals.validator';
 import { Controller } from '@hals/common';
 import { JournalsController } from './journals.controller';
+import { MongoUsersMetadataRepository } from '../users/mongo-users-metadata-repository.service';
 
 const journalsService : JournalsService = JournalsService(
    MongoJournalsRepository,
-   JournalsValidator(MongoJournalsRepository, MongoUsersRepository)
+   JournalsValidator(MongoJournalsRepository, MongoUsersMetadataRepository)
 );
 const journalsController : Controller = JournalsController(journalsService);
 
